f69bae3a195e7c7f7899b2d7db36859e948537bd
281677160 <66651997+281677160@users.noreply.github.com>
2025-05-25 18:44:32 +0800

    Update action.js

--- action.js
+++ action.js
@@ -1,32 +1,51 @@
 const core = require('@actions/core');
+const path = require('path');
+const { execFile } = require('child_process');
 
 function run() {
   try {
-    // This is just a thin wrapper around bash
-    const script = require('path').resolve(__dirname, 'script.sh');
+    // 获取输入变量
+    const telegaramToken = core.getInput('telegaram_token', { required: false });
+    const telegaramId = core.getInput('telegaram_id', { required: false });
+    const pushToken = core.getInput('push_token', { required: false });
+    const notificationCode = core.getInput('notification_code', { required: false });
+    const ghToken = core.getInput('gh_token', { required: false });
 
-    var child = require('child_process').execFile(script);
-    // child.stderr.on('data', (data) => {
-    //   console.warn(data.toString());
-    // });
-    var all_output = "";
+    // 构建环境变量
+    const env = {
+      ...process.env, // 保留当前环境变量
+      TELEGRAM_BOT_TOKEN: telegaramToken,
+      TELEGRAM_CHAT_ID: telegaramId,
+      PUSH_PLUS_TOKEN: pushToken,
+      INFORMATION_NOTICE: notificationCode,
+      GITHUB_TOKEN: ghToken
+    };
+
+    // 执行 script.sh
+    const scriptPath = path.resolve(__dirname, 'script.sh');
+    const child = execFile(scriptPath, { env });
+
+    let allOutput = '';
     child.stdout.on('data', (data) => {
-      all_output += data.toString();
+      allOutput += data.toString();
       process.stdout.write(data.toString());
     });
+
     child.stderr.on('data', (data) => {
-      all_output += data.toString();
+      allOutput += data.toString();
+      process.stderr.write(data.toString());
     });
 
     child.on('close', (code) => {
       console.log(`child process exited with code ${code}`);
-      if (code != 0) console.error("Error detected. All stdout and stderr outputs:\n" + all_output + "\n");
-      process.exit(code);
+      if (code !== 0) {
+        console.error("Error detected. All stdout and stderr outputs:\n" + allOutput + "\n");
+        process.exit(code);
+      }
     });
-  }
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
